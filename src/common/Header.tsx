f37a78be20893eb416a1483e048ab531080529a0
dudeoeoeo <dudekekek@naver.com>
2022-01-13 09:14:57 +0900

    'typescript_styled'

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Responsive from './Responsive';
+import Button from './Button';
 
 const HeaderBlock = styled.div`
     position: fixed;
